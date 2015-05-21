00d75e8caeb3a4b11bc42df70e8db07fce05770f
Aronne Brivio <aroblu94@gmail.com>
2015-05-21 09:46:50 +0200

    ...

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 function draw(){
 	var canvas = document.getElementById('tutorial');
-	if (canvas.getContext) {
+	if (canvas.getContext) {	// Check for compatibility
 		var ctx = canvas.getContext("2d");
 		
-		/* Uncomment to see rectangles
+		// Uncomment to see rectangles
+		/*
 		ctx.fillStyle = "rgb(200,0,0)";
 		ctx.fillRect (100, 100, 55, 50);
 
 		ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
 		ctx.fillRect (130, 130, 55, 50);
-		
-		* You can use:
-		* fillRect - draws a filled rectangle
-		* clearRect - draws a rectangle and erasing the background
-		* strokeRect - draws a rectangular outline
 		*/
+		
+		/*
+		 * You can use:
+		 * fillRect - draws a filled rectangle
+		 * clearRect - draws a rectangle and erasing the background
+		 * strokeRect - draws a rectangular outline
+		 */
 	}
 }
 
